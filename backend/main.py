138e97a449effde8e13b0255519650108c7b6ec4
Samuel Silva <samuelsilvawb@gmail.com>
2024-03-22 22:41:24 +0000

    feat: update BE

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,6 @@
 """
-    This is the main starting point of the backend
+    This is the main starting point of the backend. 
+
+    This is the first file that will be executed when the backend is started.
 """
 print("Hello world from python world - 3")
